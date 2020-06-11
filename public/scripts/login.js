0c7981a42e3a45c3c8217a58d88eb58bddae4674
karan <ashiwalkaran@gmail.com>
2020-06-11 16:49:20 +0530

    'login'

---
+++ public/scripts/login.js
@@ -0,0 +1,17 @@
+var email=document.getElementById("email");
+var password=document.getElementById("password");
+
+$(function(){
+  $.ajax({
+    type:'POST',
+    async:false,
+    url:'/login',
+    data:{email:email,password:password};
+    success:function(data){
+      console.log("login completed");
+    }
+    error:function(req,res){
+      res.sendFile(__dirname+'/login');
+    }
+  });
+});
